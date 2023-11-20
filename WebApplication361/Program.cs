510fb9d8ed29ab78fecc368ded30a69b8c1288c0
Dan Vouaux <daniv@microsoft.com>
2023-11-20 10:41:16 -0800

    Commit to trigger workflow

--- WebApplication361/Program.cs
+++ WebApplication361/Program.cs
@@ -18,7 +18,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Very Hot", "Sweltering", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
